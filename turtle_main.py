685cfd7d557c5fa1fcbf1ad11841b0f1c452956c
kenny <kenny.kwyiu@gmail.com>
2023-12-21 11:36:18 +0800

    21 Dec 2023 - first commit geometry_game

---
+++ turtle_main.py
@@ -0,0 +1,13 @@
+import turtle
+
+myturtle = turtle.Turtle()
+
+myturtle.forward(200)
+myturtle.left(90)
+myturtle.forward(300)
+myturtle.left(90)
+myturtle.forward(200)
+myturtle.left(90)
+myturtle.forward(300)
+
+turtle.done
